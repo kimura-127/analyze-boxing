cfa38c18beb201102a4c83d227490e9859843d16
木村愛哉 <kimuramanaya@kimulocal.local>
2023-12-10 21:24:37 +0900

    分析ページにFormを追加及びapp.tsxにRecoilRootを追加

---
+++ pages/components/Form.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useRecoilState } from "recoil";
+import { videoSrcState } from "@/atoms/videoSrcState";
+
+const Form = () => {
+    const [videoSrc, setVideoSrc] = useRecoilState(videoSrcState)
+    const { register, handleSubmit } = useForm<FormData>();
+    type FormData = {
+        video: FileList;
+    };
+
+    const tfReady = (videoData: any) => {
+        if (videoData) {
+            const file = videoData.video[0];
+            const videoURL = URL.createObjectURL(file);
+            setVideoSrc(videoURL);
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit(tfReady)} encType="multipart/form-data">
+            <input type="file" {...register("video")} />
+            <button type='submit'>びでお</button>
+        </form>
+    )
+}
+
+export default Form;
