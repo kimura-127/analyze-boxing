8a82160c689556a8267095df9af42dcf0ebc9b4f
木村愛哉 <kimuramanaya@kimulocal.local>
2024-01-18 16:42:30 +0900

    blazePoseでの推論処理

--- utils/knnClassifierPredict.tsx
+++ utils/knnClassifierPredict.tsx
@@ -1,11 +1,17 @@
 import { Tensor4D } from '@tensorflow/tfjs-core';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import * as tf from '@tensorflow/tfjs';
+import * as poseDetection from '@tensorflow-models/pose-detection';
 
 
-export const knnClassifierPredict = async (mobileNetModel: mobilenet.MobileNet, croppedImageData: Tensor4D, classifier: knnClassifier.KNNClassifier) => {
+export const knnClassifierPredict = async (mobileNetModel: mobilenet.MobileNet, croppedImage: Tensor4D, classifier: knnClassifier.KNNClassifier, blazePoseModel: poseDetection.PoseDetector, personName: string) => {
 
-    const activation = (mobileNetModel as any).infer(croppedImageData, 'conv_preds')
+    const activation = (mobileNetModel as any).infer(croppedImage, 'conv_preds')
     const result: any = await classifier.predictClass(activation)
-    return result.classIndex
+    if (result.label == personName && blazePoseModel) {
+        const squeezedImage = tf.squeeze(croppedImage as any, [0])
+        const result = await blazePoseModel.estimatePoses(squeezedImage as any)
+        return result
+    }
 }
